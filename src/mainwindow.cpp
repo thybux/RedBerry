3ed42e424614255d24c820147b51c8ba317c46cf
thybux <faivre.thomas88@free.fr>
2024-10-27 16:00:17 +0100

    ajustement

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -16,25 +16,21 @@ MainWindow::MainWindow(QWidget *parent)
     m_stackedWidget = new QStackedWidget(this);
     mainLayout->addWidget(m_stackedWidget);
 
-    // Créez la première page
     QWidget *page1 = new QWidget;
     QVBoxLayout *layout1 = new QVBoxLayout(page1);
     layout1->addWidget(new QLabel("Page 1", page1));
     m_navigateButton = new QPushButton("Aller à la page 2", page1);
     layout1->addWidget(m_navigateButton);
 
-    // Créez la deuxième page
     QWidget *page2 = new QWidget;
     QVBoxLayout *layout2 = new QVBoxLayout(page2);
     layout2->addWidget(new QLabel("Page 2", page2));
     QPushButton *backButton = new QPushButton("Retour à la page 1", page2);
     layout2->addWidget(backButton);
 
-    // Ajoutez les pages au QStackedWidget
     m_stackedWidget->addWidget(page1);
     m_stackedWidget->addWidget(page2);
 
-    // Connectez les boutons
     connect(m_navigateButton, &QPushButton::clicked, this, &MainWindow::onNavigateButtonClicked);
     connect(backButton, &QPushButton::clicked, [this]() { m_stackedWidget->setCurrentIndex(0); });
 
