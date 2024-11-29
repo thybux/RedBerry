1f6c432956b742a9cc14653987eaea1eadbaf763
thybux <faivre.thomas88@free.fr>
2024-11-29 14:26:25 +0100

    update

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -3,5 +3,5 @@ from PySide6.QtWidgets import QMainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Simple Window")  # Titre de la fenêtre
-        self.setGeometry(300, 300, 800, 600)  # Position et taille (x, y, largeur, hauteur)
+        self.setWindowTitle("Simple Window")
+        self.setGeometry(300, 300, 800, 600) 
